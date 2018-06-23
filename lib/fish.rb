357720d9569adf0a2901df218308136eb16936ba
cassidytrinh <cassidytrinh@gmail.com>
2018-06-23 18:24:55 +0000

    automatically backed up by learn

--- lib/fish.rb
+++ lib/fish.rb
@@ -1,3 +1,22 @@
 class Fish
   # code goes here
+  attr_accessor :name, :mood
+
+  def initialize(name, mood)
+    @name = name
+    @mood = mood
+  end
+
+  def name
+    @name
+  end
+
+  def mood=(mood)
+    @mood = mood
+  end
+
+  def mood
+    @mood
+  end
+
 end
